a09bef35d487f5338bc92717a31006ac4560c959
studhaerki7365 <146715629+studhaerki7365@users.noreply.github.com>
2023-10-14 13:33:16 +0200

    Add files via upload

---
+++ hello.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+
+addieren <- function(a,b) {
+  return(a+b)
+}
+
+subtrahieren <- function(a, b) {
+  return(a - b)
+}
